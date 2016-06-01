0c4438be75c8fd93998de2d12a28490bdfae1fc2
gbharathkumar <gbharathkumar@gmail.com>
2016-06-02 00:33:52 +0530

    major checkin to push the initial content with the ionic beta 7 base with Angular RC1.

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ var isRelease = argv.indexOf('--release') > -1;
 
 gulp.task('watch', ['clean'], function(done){
   runSequence(
-    ['sass', 'html', 'fonts', 'scripts', 'copyThirdParty'],
+    ['sass', 'html', 'fonts', 'scripts'],
     function(){
       gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
       gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
@@ -48,7 +48,7 @@ gulp.task('watch', ['clean'], function(done){
 
 gulp.task('build', ['clean'], function(done){
   runSequence(
-    ['sass', 'html', 'fonts', 'scripts', 'copyThirdParty'],
+    ['sass', 'html', 'fonts', 'scripts'],
     function(){
       buildBrowserify({
         minify: isRelease,
@@ -69,12 +69,4 @@ gulp.task('fonts', copyFonts);
 gulp.task('scripts', copyScripts);
 gulp.task('clean', function(){
   return del('www/build');
-});
-
-gulp.task('copyThirdParty', function(){
-	gulp.src('app/Theme/**/*.css')
-		.pipe(gulp.dest('www/build/css'));
-		
-	gulp.src('app/Theme/**/*.js')
-		.pipe(gulp.dest('www/build/js'));	
 });
